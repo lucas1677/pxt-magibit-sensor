93d37ba77bedf7088ccd0819882744ad370585be
lucas <lucas.shao@foxmail.com>
2018-05-09 16:41:53 +0800

    implements light,soil,sound,infrared sensor function.

--- MagibitSensor.ts
+++ MagibitSensor.ts
@@ -5,26 +5,37 @@
 namespace MagibitSensor {
 
     export enum LightSensorPins {
-        P0 = AnalogPin.P0,
-        P1 = AnalogPin.P1,
-        P2 = AnalogPin.P2
+        P0,
+        P1,
+        P2
     }
 
-    export enum SensorPins {
-        P0 = AnalogPin.P0,
-        P1 = AnalogPin.P1,
-        P2 = AnalogPin.P2,
-        P13 = AnalogPin.P13,
-        P16 = AnalogPin.P16
+    export enum SoundSensorPins {
+        P0,
+        P1,
+        P2
+    }
+
+    export enum SoilSensorPins {
+        P0,
+        P1,
+        P2
+    }
+
+    export enum InfraredSensorPins {
+        P13,
+        P16
     }
 
     /**
-     * read light sensor's lightness
+     * read light sensor's value
+     * @param pin sensor's active pin
+     * @return number returns analog value from 0 to 1023
      */
-    //% blockId=magibit_sensor_light_sensor_read
+    //% blockId=magibit_sensor_light_read
     //% block="Light level read analog at |%pin|"
     //% weight=80
-    export function lightSensorReadLightness(pin: LightSensorPins): number {
+    export function lightSensorReadValue(pin: LightSensorPins): number {
         switch (pin) {
             case LightSensorPins.P0:
                 return pins.analogReadPin(AnalogPin.P0);
@@ -32,6 +43,67 @@ namespace MagibitSensor {
                 return pins.analogReadPin(AnalogPin.P1);
             case LightSensorPins.P2:
                 return pins.analogReadPin(AnalogPin.P2);
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * read sound sensor's value
+     * @param pin sensor's active pin
+     * @return number returns analog value from 0 to 1023
+     */
+    //% blockId=magibit_sensor_sound_read
+    //% block="Amplitude of sound read analog at |%pin|"
+    //% weight=80
+    export function SoundSensorReadValue(pin: SoundSensorPins): number {
+        switch (pin) {
+            case SoundSensorPins.P0:
+                return pins.analogReadPin(AnalogPin.P0);
+            case SoundSensorPins.P1:
+                return pins.analogReadPin(AnalogPin.P1);
+            case SoundSensorPins.P2:
+                return pins.analogReadPin(AnalogPin.P2);
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * read soil sensor's value
+     * @param pin sensor's active pin
+     * @return number returns analog value from 0 to 1023
+     */
+    //% blockId=magibit_sensor_soil_read
+    //% block="Soil moisture read analog at |%pin|"
+    //% weight=80
+    export function SoilSensorReadValue(pin: SoilSensorPins): number {
+        switch (pin) {
+            case SoilSensorPins.P0:
+                return pins.analogReadPin(AnalogPin.P0);
+            case SoilSensorPins.P1:
+                return pins.analogReadPin(AnalogPin.P1);
+            case SoilSensorPins.P2:
+                return pins.analogReadPin(AnalogPin.P2);
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * read infrared sensor's value
+     * @param pin sensor's active pin
+     * @return number returns digital value from 0 to 1
+     */
+    //% blockId=magibit_sensor_infrared_read
+    //% block="infrared read digital at |%pin|"
+    //% weight=80
+    export function InfraredSensorReadValue(pin: InfraredSensorPins): number {
+        switch (pin) {
+            case InfraredSensorPins.P13:
+                return pins.digitalReadPin(DigitalPin.P13);
+            case InfraredSensorPins.P16:
+                return pins.digitalReadPin(DigitalPin.P16);
             default:
                 return 0;
         }
